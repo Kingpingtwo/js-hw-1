80d26d90a91236e4bb9c66b1cf971bfcc8a2f093
Kingpingtwo <108818695+Kingpingtwo@users.noreply.github.com>
2023-02-10 18:57:09 +0200

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,320 @@
+// Задача №1
+// Prompt
+// Спитати у користувача ім’я, пошту,  телефон
+// 1) Спитати у юзера імя prompt()
+// 2) Спитати у юзера пошту prompt()
+// 3) Спитати у юзера телефон prompt()
+
+// console.log(prompt("Введіть імя"));
+// console.log(prompt("Введіть пошту"));
+// console.log(prompt('Ввведіть телефон'));
+console.log(prompt("Введіть імя?"));
+console.log(prompt("Введіть пошту?"));
+console.log(prompt("Ввведіть телефон?"));
+
+// 1.1
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, адресу доставки, телефон
+console.log(prompt("Ввведіть імя?"));
+console.log(prompt("Ввведіть прізвище?"));
+console.log(prompt("Ввведіть адресу доставки?"));
+console.log(prompt("Ввведіть телефон?"));
+
+// 1.2
+// Prompt
+// Спитати у користувача автор книжки, назва, який формат електронний чи друкований
+// console.log(prompt('Який автор книги?'));
+// console.log(prompt('Яка назва твору?'));
+// console.log(prompt("Який формат електронний чи друкований?"));
+
+console.log(prompt("Автор книги?"));
+console.log(prompt("Назва книги?"));
+console.log(prompt("Який формат?"));
+
+// 1.3
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, навчальний заклад де навчається
+console.log(prompt("Ввведіть імя?"));
+console.log(prompt("Ввведіть прізвище?"));
+console.log(prompt("Ввведіть навчальний заклад?"));
+
+// 1.4
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, ким працює, де працює
+console.log(prompt("Ввведіть імя?"));
+console.log(prompt("Ввведіть прізвище?"));
+console.log(prompt("Ввведіть ким працюєте?"));
+
+// 1.5
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, адресу проживання, телефон
+console.log(prompt("Ввведіть імя?"));
+console.log(prompt("Ввведіть прізвище?"));
+console.log(prompt("Ввведіть адресу?"));
+console.log(prompt("Ввведіть телефон?"));
+/////////////////////////////////////////////////////////////////////////
+
+// Задача №2
+// Визначення типу значення, оператор typeof
+
+// const productName = typeof 'Mango';
+// const pricePerItem =typeof 150;
+// const isOnSale = typeof false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+const productName = typeof "Mango";
+const pricePerItem = typeof 150;
+const isOnSale = typeof false;
+
+console.log(productName);
+console.log(pricePerItem);
+console.log(isOnSale);
+// 2.1
+// Визначення типу значення, оператор typeof
+
+// const productName = typeof 'Ice cream';
+// const pricePerItem = typeof 70;
+// const isOnSale = typeof false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+const productName = typeof "Ice cream";
+const pricePerItem = typeof 70;
+const isOnSale = typeof false;
+
+console.log(productName);
+console.log(pricePerItem);
+console.log(isOnSale);
+
+// 2.2
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Phone';
+// const pricePerItem = 15 000;
+// const isOnSale = true;
+
+ const productName = 'Phone';
+ const pricePerItem = 15 000;
+ const isOnSale = true;
+
+// 2.3
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Bike';
+// const pricePerItem = 10 000;
+// const isOnSale = false;
+
+ const productName = 'Bike';
+ const pricePerItem = 10 000;
+ const isOnSale = false;
+
+// 2.4
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Headphone';
+// const pricePerItem = 1 000;
+// const isOnSale = false;
+
+ const productName = 'Headphone';
+ const pricePerItem = 1 000;
+ const isOnSale = false;
+
+// 2.5
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Pizza';
+// const pricePerItem = 300;
+// const isOnSale = true;
+
+ const productName = 'Pizza';
+ const pricePerItem = 300;
+ const isOnSale = true;
+
+//////////////////////////////////////////////////////////////
+
+// Задача №3
+
+// Оголоси дві змінні, productName для назви товару і pricePerItem
+// для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// 1) назва - строка "Droid"
+// 2) ціна за штуку - число 2000
+
+// 3) і вивести дані в консоль
+
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+const productName = "Droid";
+const pricePerItem = 2000;
+
+// console.log('назва продукту:', productName);
+// console.log("ціна:", pricePerItem);
+
+// 3.1
+// Оголоси дві змінні, productName для назви
+// товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Ice cream"
+// const productName = "Ice cream";
+// ціна за штуку - число 70
+// const pricePerItem = 70;
+
+const productName = "Ice cream";
+const pricePerItem = 70;
+
+// і вивести дані в консоль
+// console.log("назва товару:", productName);
+// console.log("ціна:", pricePerItem);
+
+// 3.2
+// Оголоси дві змінні, productName для назви товару
+//  і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Phone"
+// ціна за штуку - число 15 000
+
+// і вивести дані в консоль
+
+// const productName = "Phone";
+// const pricePerItem = 15000;
+
+
+const productName = "Phone";
+const pricePerItem = 15000;
+
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+// 3.3
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Headphone"
+// ціна за штуку - число 1 000
+
+const productName = "Headphone";
+const pricePerItem = 1000;
+
+// і вивести дані в консоль
+
+// 3.4
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Pizza"
+// ціна за штуку - число 300
+
+const productName = "Pizza";
+const pricePerItem = 300;
+
+// і вивести дані в консоль
+
+// 3.5
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "T-shirt"
+// ціна за штуку - число 1 200
+
+const productName = "T-shirt";
+const pricePerItem = 1200;
+
+
+// і вивести дані в консоль
+
+////////////////////////////////////////////////////////////////
+
+// Задача №4
+// Ім'я товару змінили на "Repair droid" і збільшили його ціну на 1500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Droid";
+// productName = "Repair droid";
+// console.log('назва', productName);
+
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+// console.log('назва', pricePerItem);
+
+// 4.1
+// Ім'я товару змінили на "Ice cream cones"
+// і збільшили його ціну на 150 кредитів.
+// Перевизнач значення змінних pricePerItem
+//  productName після їх оголошення.
+
+// let productName = "Ice cream";
+// productName = "Ice cream cones";
+
+// let pricePerItem = 70;
+// pricePerItem = 220;
+
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+// 4.2
+// Ім'я товару змінили на "Motorola" і збільшили його ціну на 2500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Phone";
+// productName = "Motorola";
+// console.log("назва:", productName);
+
+// let pricePerItem = 15000;
+// pricePerItem = 17500;
+// console.log("Ціна:", pricePerItem);
+
+// 4.3
+// Ім'я товару змінили на "AirProds" і збільшили його ціну на 500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Headphone";
+// let pricePerItem = 1 000;
+
+// 4.4
+// Ім'я товару змінили на "Hot Dog" і зменшили його ціну на 50 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Pizza";
+// let pricePerItem = 300;
+
+// 4.5
+// Ім'я товару змінили на "Sweater" і збільшили його ціну на 500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "T-shirt";
+// let pricePerItem = 1 200;
+
+///////////////////////////////////////////////////////////////////////////
+
+// Задача №5
+// Оголоси наступні змінні, використовуючи
+//  ключове слово const або let,
+// і присвой їм відповідні значення.
+// Виведи дані в консоль.
+
+//topSpeed - число 160.
+// distance - число 617.54.
+// login - рядок "mango935".
+// isOnline - буль true.
+// isAdmin - буль false.
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
